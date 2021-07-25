eabdc7299ca92276528aab37cc0d3424a1471809
caliglacy <85058022+caliglacy@users.noreply.github.com>
2021-07-25 20:08:26 +0900

    ✨ 記事一覧を追加

--- src/App.js
+++ src/App.js
@@ -2,16 +2,6 @@ import "./App.css";
 import { TopPage } from "./pages/TopPage";
 
 function App() {
-  // テスト：単一のコンテンツをAPIで取得する
-  const API_KEY_MICROCMS = "89355ccb-6236-420b-aeac-f740c233f765";
-  fetch("https://caliglacy.microcms.io/api/v1/test/yt3gq56pr5t", {
-    headers: {
-      "X-API-KEY": API_KEY_MICROCMS,
-    },
-  })
-    .then((res) => res.json())
-    .then((json) => console.log(json));
-
   return (
     <div>
       <TopPage />
--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,5 +1,54 @@
+import { useState, useEffect } from "react";
+import noImg from "../img/no-image.png";
+
 const TopPage = () => {
-  return <div>テスト</div>;
+  const API_KEY_MICROCMS = "89355ccb-6236-420b-aeac-f740c233f765";
+  const [posts, setPosts] = useState([]); // 記事一覧用のstate
+
+  const getPosts = () => {
+    console.log("posts", posts);
+
+    return (
+      <div>
+        {posts.map((post, index) => {
+          console.log("post", post);
+
+          return (
+            <div key={index}>
+              <div>{post.title}</div>
+              <div>
+                <span>{post.publishedAt.slice(0, 10)}</span>
+                <span>{post.category.category}</span>
+              </div>
+              <div>
+                <span>{post.thumbnail ? <img src={post.thumbnail.url}></img> : <img src={noImg}></img>}</span>
+              </div>
+              <div>{post.description}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    // 投稿を取得する(全取得、新しい順)
+    fetch("https://caliglacy.microcms.io/api/v1/post/", {
+      headers: {
+        "X-API-KEY": API_KEY_MICROCMS,
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => setPosts(json.contents))
+      .catch((err) => console.log("投稿データの取得に失敗しました。 TopPage.js"));
+  }, []);
+
+  return (
+    <div>
+      <h2>投稿一覧の表示テスト</h2>
+      {getPosts()}
+    </div>
+  );
 };
 
 export { TopPage };
