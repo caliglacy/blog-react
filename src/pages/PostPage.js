187010614d45afb994ace612594b8d9eb31e8e0b
caliglacy <85058022+caliglacy@users.noreply.github.com>
2021-07-31 21:28:25 +0900

    🚸 ローディング画面を追加

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
+import { Loading } from "../components/Loading";
 import { PostAll } from "../components/PostAll";
 
 const PostPage = (props) => {
   const id = props.match.params.id; // 記事IDをprops(URL末尾)から取得
   const API_KEY_MICROCMS = "89355ccb-6236-420b-aeac-f740c233f765"; // ToDo: 分散しているAPIキー定義は一か所にまとめておきたい
+
+  const [loading, setLoading] = useState(false); // 記事データ取得中
 
   // 記事データのstate
   const [post, setPost] = useState({
@@ -17,6 +20,9 @@ const PostPage = (props) => {
 
   // コンポーネント表示時に記事を取得
   useEffect(() => {
+    // 記事データ取得開始
+    setLoading(true);
+
     // 投稿を取得する
     fetch(`https://caliglacy.microcms.io/api/v1/post/${id}`, {
       headers: {
@@ -33,15 +39,17 @@ const PostPage = (props) => {
           description: data.description,
           body: data.body,
         });
+        // 記事データ取得成功
+        setLoading(false);
       })
-      .catch((err) => console.log("投稿データの取得に失敗しました。 PostPage.js", err));
+      .catch((err) => {
+        console.log("投稿データの取得に失敗しました。 PostPage.js", err);
+        // 記事データ取得失敗
+        setLoading(false);
+      });
   }, []);
 
-  return (
-    <div>
-      <PostAll props={post} />
-    </div>
-  );
+  return <div>{loading ? <Loading /> : <PostAll props={post} />}</div>;
 };
 
 export { PostPage };
