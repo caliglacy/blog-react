336c7ebc7ae0e6e6700b8a8ca284fc64f1a411f7
caliglacy <85058022+caliglacy@users.noreply.github.com>
2021-08-01 10:31:20 +0900

    ♻️ 最新記事一覧をコンポーネント化

---
+++ src/components/LatestPosts.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import { PostAbout } from "../components/PostAbout";
+import { Loading } from "./Loading";
+
+const LatestPosts = () => {
+  // このコンポーネントは最新の投稿を表示します
+
+  const API_KEY_MICROCMS = "89355ccb-6236-420b-aeac-f740c233f765";
+  const [posts, setPosts] = useState([]); // 記事一覧用のstate
+  const [loading, setLoading] = useState(false); // 記事データ取得中
+
+  useEffect(() => {
+    // 記事データ取得開始
+    setLoading(true);
+
+    // 投稿を取得する(全取得、新しい順)
+    fetch("https://caliglacy.microcms.io/api/v1/post/", {
+      headers: {
+        "X-API-KEY": API_KEY_MICROCMS,
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        setPosts(json.contents);
+        // 記事データ取得成功
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log("投稿データの取得に失敗しました。 TopPage.js");
+        // 記事データ取得失敗
+        setLoading(false);
+      });
+  }, []);
+
+  const getPosts = () => {
+    return (
+      <div>
+        {posts.map((post, index) => {
+          return (
+            <div key={index}>
+              <PostAbout props={post} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  return <div>{loading ? <Loading /> : getPosts()}</div>;
+};
+
+export { LatestPosts };
--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,54 +1,10 @@
-import { useState, useEffect } from "react";
-import { Loading } from "../components/Loading";
-import { PostAbout } from "../components/PostAbout";
+import { LatestPosts } from "../components/LatestPosts";
 
 const TopPage = () => {
-  const API_KEY_MICROCMS = "89355ccb-6236-420b-aeac-f740c233f765";
-  const [posts, setPosts] = useState([]); // 記事一覧用のstate
-
-  const [loading, setLoading] = useState(false); // 記事データ取得中
-
-  const getPosts = () => {
-    return (
-      <div>
-        {posts.map((post, index) => {
-          return (
-            <div key={index}>
-              <PostAbout props={post} />
-            </div>
-          );
-        })}
-      </div>
-    );
-  };
-
-  useEffect(() => {
-    // 記事データ取得開始
-    setLoading(true);
-
-    // 投稿を取得する(全取得、新しい順)
-    fetch("https://caliglacy.microcms.io/api/v1/post/", {
-      headers: {
-        "X-API-KEY": API_KEY_MICROCMS,
-      },
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        setPosts(json.contents);
-        // 記事データ取得成功
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log("投稿データの取得に失敗しました。 TopPage.js");
-        // 記事データ取得失敗
-        setLoading(false);
-      });
-  }, []);
-
   return (
     <div>
       <h2>コンポーネント：トップ</h2>
-      {loading ? <Loading /> : getPosts()}
+      <LatestPosts />
     </div>
   );
 };
