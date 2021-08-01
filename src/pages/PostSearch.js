2467a64014df728b274532d1d65632ab1d8b75bc
caliglacy <85058022+caliglacy@users.noreply.github.com>
2021-08-01 19:19:22 +0900

    ✅ テキストボックスと検索ボタンを配置

--- src/pages/PostSearch.js
+++ src/pages/PostSearch.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 const PostSearch = () => {
-  return <div>記事検索用のページです。</div>;
+  const [keyword, setKeyword] = useState("");
+
+  const searchKeyword = (word) => {
+    // デバッグ処理
+    console.log(word);
+  };
+
+  return (
+    <div>
+      <div>キーワードで検索</div>
+      <input type="text" onChange={(e) => setKeyword(e.target.value)}></input>
+      <button onClick={() => searchKeyword(keyword)}>検索</button>
+    </div>
+  );
 };
 
 export { PostSearch };
