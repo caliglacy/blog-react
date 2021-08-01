e611382c4fa448510c93ecff11e76fa854d73227
caliglacy <85058022+caliglacy@users.noreply.github.com>
2021-08-01 17:41:54 +0900

    ✨ ヘッダーのコンテンツを追加

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,22 @@
+import { BrowserRouter, Link } from "react-router-dom";
 const Header = () => {
-  return <div>これはヘッダーです</div>;
+  return (
+    <div>
+      <div>ロゴ</div>
+      <ul>
+        <li>
+          <Link activeClassName="active" exact to="/">
+            ホーム
+          </Link>
+        </li>
+        <li>
+          <Link activeClassName="active" exact to="/post">
+            投稿検索
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export { Header };
