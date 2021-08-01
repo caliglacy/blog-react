453bfb2ccad72dfab4bb570a3e66fa237f19ba1e
caliglacy <85058022+caliglacy@users.noreply.github.com>
2021-08-01 16:11:00 +0900

    ✨ ヘッダーを追加

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import { TopPage } from "./pages/TopPage";
 import { PostPage } from "./pages/PostPage";
 import { Page404 } from "./pages/Page404";
 import { PostSearch } from "./pages/PostSearch";
+import { Header } from "./components/Header";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={TopPage} />
-        {/* <Route exact path="/post" component={PostListPage} /> */}
-        <Route exact path="/post" component={PostSearch /* ToDo: 仮配置。設計通りであるかはチェックすること。 */} />
-        <Route path="/post/:id" component={PostPage} />
-        <Route component={Page404} />
-      </Switch>
-    </BrowserRouter>
+    <div>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={TopPage} />
+          {/* <Route exact path="/post" component={PostListPage} /> */}
+          <Route exact path="/post" component={PostSearch /* ToDo: 仮配置。設計通りであるかはチェックすること。 */} />
+          <Route path="/post/:id" component={PostPage} />
+          <Route component={Page404} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
