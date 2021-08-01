38a7800dbb443615ac7c0acab62997d6a2e3588d
caliglacy <85058022+caliglacy@users.noreply.github.com>
2021-08-01 22:05:34 +0900

    ✨ カテゴリ一覧の表示を追加

---
+++ src/components/ShowCategories.js
@@ -0,0 +1,16 @@
+const ShowCategories = ({ categories }) => {
+  console.log(categories);
+  //カテゴリを表示
+  return categories.length ? (
+    <div>
+      {categories.map((category, index) => {
+        return <span key={"category-" + String(index)}>{category.category}</span>;
+      })}
+    </div>
+  ) : (
+    // 表示するカテゴリが0個だった場合(特に表示するものなし)
+    <div></div>
+  );
+};
+
+export { ShowCategories };
--- src/pages/PostSearch.js
+++ src/pages/PostSearch.js
@@ -1,11 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Loading } from "../components/Loading";
+import { ShowCategories } from "../components/ShowCategories";
 import { ShowPosts } from "../components/ShowPosts";
 
 const PostSearch = () => {
   const [keyword, setKeyword] = useState("");
+  const [categories, setCategories] = useState("");
   const [posts, setPosts] = useState([]); // 記事一覧用のstate
   const [loading, setLoading] = useState(false); // 記事データ取得中
+
+  useEffect(() => {
+    // リクエスト情報作成
+    const API_KEY_MICROCMS = "89355ccb-6236-420b-aeac-f740c233f765";
+    const url = new URL("https://caliglacy.microcms.io/api/v1/category/");
+    // カテゴリ一覧を取得
+    fetch(url, {
+      headers: {
+        "X-API-KEY": API_KEY_MICROCMS,
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        setCategories(json.contents);
+      })
+      .catch((err) => {
+        console.log("カテゴリ一覧の取得に失敗しました。", err);
+      });
+  }, []);
 
   const searchKeyword = (word) => {
     // 記事データ取得開始
@@ -38,10 +59,16 @@ const PostSearch = () => {
 
   return (
     <div>
-      <div>キーワードで検索</div>
-      <input type="text" onChange={(e) => setKeyword(e.target.value)}></input>
-      <button onClick={() => searchKeyword(keyword)}>検索</button>
-      {loading ? <Loading /> : <ShowPosts posts={posts} />}
+      <div>
+        <div>キーワードで検索</div>
+        <input type="text" onChange={(e) => setKeyword(e.target.value)}></input>
+        <button onClick={() => searchKeyword(keyword)}>検索</button>
+        {loading ? <Loading /> : <ShowPosts posts={posts} />}
+      </div>
+      <div>
+        <div>カテゴリで検索</div>
+        <ShowCategories categories={categories} />
+      </div>
     </div>
   );
 };
