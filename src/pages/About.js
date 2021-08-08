a391f22fed4a18f0ac2ea96bb0ac0c0a1242f1a4
caliglacy <85058022+caliglacy@users.noreply.github.com>
2021-08-08 19:45:13 +0900

    ✨ ブログ説明の表示処理を追加

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,43 @@
+import info from "../data/about.json";
+
 const About = () => {
-  return <div>このブログについて</div>;
+  return (
+    <div>
+      <div>
+        <h2>{info.what.title}</h2>
+        <div dangerouslySetInnerHTML={{ __html: info.what.body }} />
+      </div>
+      <div>
+        <h2>{info.purpose.title}</h2>
+        <ul>
+          {info.purpose.point.map((item, key) => {
+            return <li key={key}>{item}</li>;
+          })}
+        </ul>
+        <div dangerouslySetInnerHTML={{ __html: info.purpose.body }} />
+      </div>
+      <div>
+        <h2>{info.technology.title}</h2>
+        <ul>
+          {info.technology.list.map((item, key) => {
+            return (
+              <li key={key}>
+                {item.category}：{item.item}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div>
+        <h2>{info.plan.title}</h2>
+        <ul>
+          {info.plan.list.map((item, key) => {
+            return <li key={key}>{item}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export { About };
