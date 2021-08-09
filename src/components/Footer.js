5f607478003fae6cde1dbbf5f77db791019c8f5f
caliglacy <85058022+caliglacy@users.noreply.github.com>
2021-08-09 18:17:00 +0900

    💄 フッター上部にマージンを追加

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   const Year = new Date().getFullYear(); // 表示時の西暦
   return (
-    <div className="py-2 bg-purple-900 text-white">
+    <div className="mt-4 py-2 bg-purple-900 text-white">
       <div className="flex justify-center space-x-8">
         <span className="">ホーム</span>
         <span className="">先頭に戻る</span>
