30cf44ce843a7bfaa5c0cffba18dbb9d515e6c35
caliglacy <85058022+caliglacy@users.noreply.github.com>
2021-08-09 16:44:51 +0900

    💄 フッターにスタイルを適用

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 const Footer = () => {
   const Year = new Date().getFullYear(); // 表示時の西暦
   return (
-    <div>
-      <div>
-        <span>ホーム</span>
-        <span>先頭に戻る</span>
-        <span>問い合わせ</span>
+    <div className="py-2 bg-purple-900 text-white">
+      <div className="flex justify-center space-x-8">
+        <span className="">ホーム</span>
+        <span className="">先頭に戻る</span>
+        <span className="">問い合わせ</span>
       </div>
-      <div>© {Year} caliglacy All rights reserved.</div>
+      <div className="text-center">© {Year} caliglacy All rights reserved.</div>
     </div>
   );
 };
