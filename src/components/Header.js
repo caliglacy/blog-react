1a2906000282a9d469262aeaa8adc047574f0e97
caliglacy <85058022+caliglacy@users.noreply.github.com>
2021-08-09 16:25:58 +0900

    💄 ヘッダにスタイルシートを適用

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,53 @@
 import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
-    <div>
-      <div>かりらぼ。</div>
-      <ul>
-        <li>
-          <NavLink activeClassName="active" exact to="/">
+    <div className="bg-indigo-100 flex">
+      <div className="flex-auto py-3 text-4xl ">かりらぼ。</div>
+      <ul className="h-auto flex-auto flex ">
+        <li className="flex-auto w-18 hover:bg-indigo-300 duration-200">
+          <NavLink
+            className="block h-full text-lg"
+            activeClassName=" active bg-purple-600 text-white font-semibold"
+            exact
+            to="/"
+          >
             ホーム
           </NavLink>
         </li>
-        <li>
-          <NavLink activeClassName="active" exact to="/post">
+        <li className="flex-auto w-18 hover:bg-indigo-300 duration-200">
+          <NavLink
+            className="block h-full text-lg"
+            activeClassName="active bg-purple-600 text-white font-semibold"
+            exact
+            to="/post"
+          >
             投稿検索
           </NavLink>
         </li>
-        <li>
-          <NavLink activeClassName="active" to="/profile">
+        <li className="flex-auto w-18 hover:bg-indigo-300 duration-200">
+          <NavLink
+            className="block h-full text-lg"
+            activeClassName="active bg-purple-600 text-white font-semibold"
+            to="/profile"
+          >
             プロフィール
           </NavLink>
         </li>
-        <li>
-          <NavLink activeClassName="active" to="/contact">
+        <li className="flex-auto w-18 hover:bg-indigo-300 duration-200">
+          <NavLink
+            className="block h-full text-lg"
+            activeClassName="active bg-purple-600 text-white font-semibold"
+            to="/contact"
+          >
             問い合わせ
           </NavLink>
         </li>
-        <li>
-          <NavLink activeClassName="active" to="/about">
+        <li className="flex-auto w-18 hover:bg-indigo-300 duration-200">
+          <NavLink
+            className="block h-full text-lg"
+            activeClassName="active bg-purple-600 text-white font-semibold"
+            to="/about"
+          >
             このブログについて
           </NavLink>
         </li>
