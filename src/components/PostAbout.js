1b7c00753fac09c327bb7dcb253e86d0fa1e30b0
caliglacy <85058022+caliglacy@users.noreply.github.com>
2021-08-09 17:24:29 +0900

    💄 記事一覧にスタイルシートを適用

--- src/components/PostAbout.js
+++ src/components/PostAbout.js
@@ -4,22 +4,28 @@ import noImg from "../img/no-image.png";
 const PostAbout = ({ props }, index) => {
   return (
     <Link to={`/post/${props.id}`}>
-      <div key={"post-about-" + String(index)}>
-        <h3>{props.title}</h3>
-        <div>
-          <span>{props.publishedAt.slice(0, 10)}</span>
-          <span>{props.category.category}</span>
-        </div>
-        <div>
-          <span>
+      <div key={"post-about-" + String(index)} className="flex my-8 p-4 bg-indigo-50 shadow-md">
+        <div className>
+          <span className="bg-gray-300">
             {props.thumbnail ? (
-              <img src={props.thumbnail.url} alt={`thumbnail-${props.id}`}></img>
+              <img
+                src={props.thumbnail.url}
+                alt={`thumbnail-${props.id}`}
+                className="object-cover h-40 w-40 mx-6"
+              ></img>
             ) : (
-              <img src={noImg} alt="no-thumbnail"></img>
+              <img src={noImg} alt="no-thumbnail" className="object-cover h-40 w-40 mx-6"></img>
             )}
           </span>
         </div>
-        <div>{props.description}</div>
+        <div>
+          <h3 className="text-2xl font-medium mt-2 mb-2">{props.title}</h3>
+          <div className="mb-2">
+            <span className="mr-4">{props.publishedAt.slice(0, 10)}</span>
+            <span>{props.category.category}</span>
+          </div>
+          <div className="mb-2">{props.description}</div>
+        </div>
       </div>
     </Link>
   );
